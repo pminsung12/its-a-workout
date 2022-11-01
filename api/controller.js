18d27ebdaebfe15caab94a5228ecb6805236d941
Serin-Yoon <bcode429@gmail.com>
2022-11-02 00:40:51 +0900

    [FEAT] 인트로 페이지

--- api/controller.js
+++ api/controller.js
@@ -3,6 +3,5 @@ const request = require("request");
 const dao = require("./dao");
 
 exports.main = async (req, res) => {
-    res.json({result: "이제 html을 리턴해보자"});
-    // return res.render("~.ejs", {}); 하면 됨!
+    return res.render("../public/html/intro.ejs", {});
 }
