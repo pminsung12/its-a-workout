dafc328a2c369bb6699c2865faca3e0af92a938a
pminsung12 <pminsung12@gmail.com>
2022-11-04 13:46:04 +0900

    [FEAT] Ejs engine

--- api/controller.js
+++ api/controller.js
@@ -1,6 +1,20 @@
 // 요청 처리 작업
 const request = require("request");
 const dao = require("./dao");
+const User = require("../models/user");
+
+exports.getAddUser = (req, res, next) => {
+    res.render("admin/edit-product", {
+        pageTitle: "Add User",
+        path: "", //path 추가해야함.
+        editing: false,
+    });
+};
+exports.postAddUser = (req, res, next) => {
+    const name = req.body.name;
+};
+
+
 
 exports.main = async (req, res) => {
     return res.render("../public/html/intro.ejs", {});
--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 // 서버 소켓 작업 및 서버 실행
 const path = require("path");
 const http = require("http");
-const request = require("request");
+// const request = require("request");
 const controller = require("./api/controller");
+const errorController = require("./api/error");
 const dao = require("./api/dao");
 const router = require("./api/router");
+
+const User = require("./models/user");
+
 const express = require("./config/express");
 const app = express();
-const server = http.createServer(app);
 
+app.set("view engine", "ejs"); //express에 스스로를 자동으로 등록
+app.set("views", "views"); //default긴하지만 그래도 적어줌
+
+// app.use((req,res,next)=>{ //미들웨어 등록, 필요한 코드지만 
+//     User.findByPk(1) // 아직 코드를 완벽하게 이해 못해서 주석처리함.
+//     .then((user) => {
+//     req.user = user;
+//     next(); // 다음 미들웨어로
+//     })
+//     .catch((err) => console.log(err));
+// });
+app.use(router);
+app.use(errorController.get404);
 /* Socket */
 
+const server = http.createServer(app);
 const PORT = 3000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
