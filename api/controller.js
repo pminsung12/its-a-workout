948bb04fa920dfc53d0f251179a9826d9914cb84
pminsung12 <pminsung12@gmail.com>
2022-11-07 19:56:52 +0900

    [FIX] 주석제거

--- api/controller.js
+++ api/controller.js
@@ -25,39 +25,3 @@ exports.rank = async (req, res) => {
 exports.test = async (req, res) => {
     return res.render("test", { pageTitle: "Tutorial" });
 }
-
-
-/* TODO: 우선 충돌 날까봐 주석 처리 해뒀어 */
-
-// exports.getAddUser = (req, res, next) => {
-//     res.render("admin/edit-product", {
-//         pageTitle: "Add User",
-//         path: "", //path 추가해야함.
-//         editing: false,
-//     });
-// };
-
-// exports.postAddUser = (req, res, next) => {
-//     // 유저 추가 메서드
-//     const name = req.body.name;
-// };
-//
-// exports.getTutorial = (req, res, next) => {
-//     const name = req.body.name;
-// };
-//
-// exports.getPlayGame = (req, res, next) => {
-//     const name = req.body.name;
-// };
-//
-// exports.postPlayGame = (req, res, next) => {
-//     const name = req.body.name;
-// };
-//
-// exports.getRank = (req, res, next) => {
-//     const name = req.body.name;
-// };
-//
-// exports.postRank = (req, res, next) => {
-//     const name = req.body.name;
-// };
--- api/error.js
+++ api/error.js
@@ -1,6 +1,4 @@
 exports.get404 = (req, res, next) => {
-    //console.log(req.body);
-    //status는 404 상태코드 설정
     res.status(404).render("404", { pageTitle: "Page Not Found", path: "/404" });
 };
 
